a64a85e858e70bb1a440522849cf90576eb79c5a
0chroma <21014+0chroma@users.noreply.github.com>
2010-08-07 03:19:57 -0400

    initial commit

---
+++ main.h
@@ -0,0 +1,9 @@
+/*
+ * Blackpyre Engine
+ * Copyright (c) 2010 0chroma. All rights reserved.
+ * 
+ * Source available under the Academic Free License
+ * See LICENSE for licensing information
+ */
+
+ 
