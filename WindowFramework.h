5bf27a652977fad2d71744d733bbbabdb001b044
0chroma <21014+0chroma@users.noreply.github.com>
2010-08-09 03:13:14 -0400

    worked on the initial layout a bit

---
+++ WindowFramework.h
@@ -0,0 +1,20 @@
+/*
+ * Blackpyre Engine
+ * Copyright (c) 2010 0chroma. All rights reserved.
+ * 
+ * Source available under the Academic Free License
+ * See LICENSE for licensing information
+ */
+
+#ifndef WindowFramework_h
+#define WindowFramework_h
+
+class WindowFramework {
+    public:
+        WindowFramework(int argc = 0, char **argv = 0);
+        virtual ~WindowFramework();
+
+        virtual int createWindow(char title);
+}
+
+#endif // WindowFramework_h
