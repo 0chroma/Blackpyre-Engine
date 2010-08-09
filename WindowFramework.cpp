ac92d756fa91f154b2f2fc7dac049c6798c4508c
Will Riley <will@psychdesigns.net>
2010-08-09 03:13:14 -0400

    worked on the initial layout a bit

---
+++ WindowFramework.cpp
@@ -0,0 +1,17 @@
+/*
+ * Blackpyre Engine
+ * Copyright (c) 2010 William Riley. All rights reserved.
+ * 
+ * Source available under the Academic Free License
+ * See LICENSE for licensing information
+ */
+
+#include "WindowFramework.h"
+
+WindowFramework::WindowFramework(int, char **){
+
+}
+
+WindowFramework::~WindowFramework(){
+
+}
